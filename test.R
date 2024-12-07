38fe82d1169891ff083d904362fd63f735915f47
Zuzanna_Sosnowska <126651779+prog-zuzanna-sosnowska@users.noreply.github.com>
2024-12-07 14:39:26 +0100

    Adding boxplots
    Dodanie boxplotów z ogólną oceną kawy w zależności od kontynentu z którego pochodzi.

--- test.R
+++ test.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(ggplot2)
 library(tidyverse)
 library(patchwork)
+library(countrycode)
 
 data <- read.csv("arabica_data_cleaned.csv")
 data[data == ""] <- NA
@@ -13,6 +14,16 @@ data_select <- data[c("Country.of.Origin", "Processing.Method",
 data_clean <- data_select %>%
   mutate(Processing.Method = factor(Processing.Method),)
 
-# view(data_clean)
+view(data_clean)
 
-subset(data_clean, Flavor < 5 | is.na(Flavor))
+
+
+data_clean$Continent <- countrycode(sourcevar = data_clean$Country.of.Origin,
+                                    origin = "country.name",
+                                    destination = "continent")
+
+data_clean[is.na(data_clean$Continent) & !is.na(data_clean$Country.of.Origin),]$Continent <- "Americas"
+
+
+view(data_clean$Continent)
+
